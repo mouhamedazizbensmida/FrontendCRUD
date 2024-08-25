4bc5214d77d918bb0574422fa0e021ba51be1dbe
Med Aziz Ben Smida <73040491+mouhamedazizbensmida@users.noreply.github.com>
2024-08-25 22:24:55 +0200

    environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   production: false,
   /*apiBaseUrl: 'https://hostingdemospringboot-10087108309.development.catalystappsail.com'*/ 
   /*apiBaseUrl: 'http://localhost:8080'*/
-  apiBaseUrl: 'https://backendcrud-h550.onrender.com'
+  /*apiBaseUrl: 'https://backendcrud-h550.onrender.com'*/
+  apiBaseUrl: https://hostingdemospringboot-10087108309.development.catalystappsail.com
   
 };
 
